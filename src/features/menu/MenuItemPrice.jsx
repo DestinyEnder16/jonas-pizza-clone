f80bd62eec3eab3541295bd9e6a77836a0891258
Israel Destiny <destinyprogrammer06@gmail.com>
2025-09-11 18:42:37 +0100

    Changed text color of price

--- src/features/menu/MenuItemPrice.jsx
+++ src/features/menu/MenuItemPrice.jsx
@@ -2,7 +2,9 @@ import { formatCurrency } from "../../utilities/helper";
 
 function MenuItemPrice({ price, isSoldOut }) {
   return (
-    <span className={`text-3xl font-extrabold ${isSoldOut && "line-through"}`}>
+    <span
+      className={`text-3xl font-extrabold ${isSoldOut && "line-through"} text-charcoal`}
+    >
       {formatCurrency(price)}
     </span>
   );
