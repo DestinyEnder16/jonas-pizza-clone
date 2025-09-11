dd8b11b7fef01b96a111e1b1102da688276d768b
Israel Destiny <destinyprogrammer06@gmail.com>
2025-09-11 20:56:27 +0100

    Added an error element

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import { RouterProvider } from "react-router/dom";
 import Homepage from "./ui/Homepage";
 import Menu from "./features/menu/Menu";
 import { loader as menuLoader } from "./features/menu/Menu";
+import ErrorPage from "./ui/ErrorPage";
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <ErrorPage />,
     children: [
       { path: "/", element: <Homepage /> },
       { path: "/menu", element: <Menu />, loader: menuLoader },
