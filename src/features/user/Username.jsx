0c41e684f3ad2c75a7a4d2d83d11dd8c428f9670
Israel Destiny <destinyprogrammer06@gmail.com>
2025-09-11 18:52:43 +0100

    Added a new cart logo

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -2,9 +2,7 @@ import { useSelector } from "react-redux";
 
 function Username() {
   const { username } = useSelector((store) => store.user);
-  return (
-    <div className="font-medium">{!username.length ? null : username}</div>
-  );
+  return <div className="font-medium">{username}</div>;
 }
 
 export default Username;
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,18 +1,23 @@
 import { useSelector } from "react-redux";
 import { Link } from "react-router";
 import Username from "../features/user/Username";
+import Cart from "./Cart";
 
 function Header() {
   const { username } = useSelector((store) => store.user);
   return (
-    <header className="bg-crust text-md flex h-min justify-between px-3 py-4">
+    <header className="bg-crust text-md fixed top-0 z-50 flex h-min w-full justify-between px-3 py-4 shadow-md">
       <span>
         <Link to={"/"} className="font-semibold outline-none">
           Fast Pizza Co.
         </Link>
       </span>
 
-      <Username />
+      <span>
+        <Cart />
+      </span>
+
+      {username.length > 0 && <Username />}
     </header>
   );
 }
