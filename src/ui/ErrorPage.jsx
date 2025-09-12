ed108d881ed677a5fd3413efa75b13ad3570f290
Israel Destiny <destinyprogrammer06@gmail.com>
2025-09-12 13:42:37 +0100

    Created a 'back to homepage' button for error handling and navigation

--- src/ui/ErrorPage.jsx
+++ src/ui/ErrorPage.jsx
@@ -1,15 +1,18 @@
-import { useRouteError } from "react-router";
+import { Link, useRouteError } from "react-router";
+import BackHomeBtn from "./BackHomeBtn";
 
 function ErrorPage() {
   const error = useRouteError();
   console.log(error);
 
   return (
-    <div>
+    <div className="flex h-screen flex-col items-center justify-center gap-2">
       <h1>Something went wrong 😢</h1>
       <p>{error.data || error.message}</p>
 
-      {/* <LinkBut to="-1">&larr; Go back</LinkButton> */}
+      {/* <LinkBut to="-1">&larr; Go back</LinkButton>
+       */}
+      <BackHomeBtn />
     </div>
   );
 }
