364545822728ea4a0c51046b6017e5b060c3e470
Israel Destiny <destinyprogrammer06@gmail.com>
2025-09-12 19:00:40 +0100

    footer

--- src/ui/Footer.jsx
+++ src/ui/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <footer className="bg-charcoal text-md flex h-min w-full px-1 py-4 text-white">
-      <h3>Trusted by many</h3>
+      <h3>Too tired to design a proper footer.</h3>
     </footer>
   );
 }
